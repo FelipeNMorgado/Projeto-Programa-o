74478a23cd49dc21a226f5117eec0b13884e306b
FelipeNunes <felipe.nunes.morgado@gmail.com>
2023-11-12 16:41:55 -0300

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -1,52 +1,27 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-def adicionar():
-    adi = 1
-    #função para adição de livros 
-    
-
-
-livro = []
-=======
-biblioteca = {}
->>>>>>> 674c66931953d75eecb588b5abaa1d0c7f1d4f7d
-
-result = "S"
-
-
-escolha_menu = int(input("Escolha o que voce deseja realizar: \n[1]Adicionar livro \n[2]Vizualizar livros \n[3]Excluir livro \n[4]Procurar livro \n[5]Mais opções \n"))
-
-
-while result == "S":
-    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar\n4-Ler"))
-    if mod == 1:
-        option_1 = input("Voce deseja criar outra categoria?")
-        if option_1 == "S":
-            
-            
-
-
-
-
-
-
-
-<<<<<<< HEAD
-#bom dia flor do dia
-#boa noite flor da noite
-
-=======
->>>>>>> 674c66931953d75eecb588b5abaa1d0c7f1d4f7d
-=======
 biblioteca = {}
 
 result = "S"
-
+preco_total = 0
 while result == "S":
-    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar\n4-Ler"))
+    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar\n4-Visualizar\n5-Carrinho\n"))
     if mod == 1:
-        option_1 = input("Voce deseja criar outra categoria?")
-        if option_1 == "S":
+        option_1 = input("Voce deseja adicionar uma categoria('C') ou um Livro('L')?")
+        if option_1 == "C":
+            cat_add = input("Qual a categoria que deseja adicionar?")
+            biblioteca[cat_add] = ""
+            print(biblioteca)
+        elif option_1 == "L":
+            livro_name = input("Nome do livro:")
+            livro_autor = input("Autor do livro")
+            livro_categoria = input("Categoria do livro:")
+            livro_preco = int(input("Preço do livro:"))
+            livro = livro_name , livro_autor , livro_categoria , livro_preco
+            biblioteca[livro_categoria] = [livro]
+            print(biblioteca)
+            preco_total =+ livro_preco
+        else:
+            print("Operação Invalida")
+
             
             
 
@@ -56,4 +31,3 @@ while result == "S":
 
 
 
->>>>>>> 674c66931953d75eecb588b5abaa1d0c7f1d4f7d
