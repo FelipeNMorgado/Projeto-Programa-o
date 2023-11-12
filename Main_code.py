674c66931953d75eecb588b5abaa1d0c7f1d4f7d
FelipeNunes <felipe.nunes.morgado@gmail.com>
2023-11-12 16:01:39 -0300

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -1,15 +1,18 @@
-livro = []
+biblioteca = {}
 
 result = "S"
 
 while result == "S":
-    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar"))
-    
+    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar\n4-Ler"))
+    if mod == 1:
+        option_1 = input("Voce deseja criar outra categoria?")
+        if option_1 == "S":
+            
+            
 
 
-categoria_livro = ""
-nome_livro = ""
-preco_livro = 0
-autor_livro = ""    
 
-#bom dia flor do dia
+
+
+
+
