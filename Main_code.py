972944a9e9c1d014b7ee28df694c244904d62a2f
Breno-Lira <bmrl@cesar.school>
2023-11-12 15:25:30 -0300

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -2,6 +2,10 @@ livro = []
 
 result = "S"
 
+
+escolha_menu = int(input("Escolha o que voce deseja realizar: \n[1]Adicionar livro \n[2]Vizualizar livros \n[3]Excluir livro \n[4]Procurar livro \n[5]Mais opções \n"))
+
+
 while result == "S":
     mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar"))
     
