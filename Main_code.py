bf362977b4f0c2631e79efd10d362532194973d3
FelipeNunes <felipe.nunes.morgado@gmail.com>
2023-11-12 14:10:59 -0300

    Create Main_code.py

---
+++ Main_code.py
@@ -0,0 +1,13 @@
+livro = []
+
+result = "S"
+
+while result == "S":
+    mod = int(input("Precione o numero correspondente a alteração que deseja:\n1-Adicionar\n2-Remover\n3-Alterar"))
+    
+
+
+categoria_livro = ""
+nome_livro = ""
+preco_livro = 0
+autor_livro = ""    
