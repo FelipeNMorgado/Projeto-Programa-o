6b42cf30e10aa7da2dc1b4c53d41794cf1c6da34
LucasHolandaBarros <142419627+LucasHolandaBarros@users.noreply.github.com>
2023-11-15 18:30:44 -0300

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -2,26 +2,29 @@ import os
 os.system("cls")
 
 biblioteca = {}
-preco_total = 0
 lista_cat = []
 cont = 0
+cont_remove = 0
+soma = 0
+cont_mudar = 0
 
 while True:
     print("--" * 40)
-    mod = int(input("Pressione o número correspondente à alteração que deseja:\n\n[1]-Adicionar\n[2]-Excluir\n[3]-Atualizar\n[4]-Visualizar\n[5]-Dinheiro Gasto\n[6]-Parar\n\nDigite aqui a sua opção: "))
+    mod = int(input("Pressione o número correspondente à alteração que deseja:\n\n[1]-Adicionar\n[2]-Excluir\n[3]-Atualizar\n[4]-Visualizar\n[5]-Dinheiro Gasto\n[6]-Carrinho\n[7]-Parar\n\nDigite aqui a sua opção: "))
     os.system("cls")
     
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
     
-    if mod == 1:
+    if mod == 1: 
         os.system("cls")
+        
         cat_add = input("Categoria do livro: ")
         livro_name = input("Nome do livro: ")
         livro_autor = input("Autor do livro: ")
         livro_preco = int(input("Preço do livro: "))
         livro = {"nome": livro_name, "autor": livro_autor, "preço": livro_preco}
         print("\nLivro adicionado com sucesso!")
-        
+
         if cat_add in biblioteca:
             biblioteca[cat_add].append(livro)
         else:
@@ -31,16 +34,15 @@ while True:
             continue
         else:
             lista_cat.append(cat_add)
-        
-        cont += 1
-
-        preco_total += livro_preco
-        
+            cont += 1
+            
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
     
     elif mod == 2:
         os.system("cls")
-        
+
+        cont_remove = 0
+
         print("Categorias disponiveis: \n")
 
         for x in range(cont):
@@ -56,22 +58,59 @@ while True:
                 print(f"Nome: {livro['nome']}")
                 
             livro_remove = input("\nDigite o livro que você deseja remover: ")
-                
-            if livro['nome'] == livro_remove:
-                biblioteca[cat_remove].remove(livro)
-                print(f"\nLivro '{livro_remove}' removido com sucesso!")
-                preco_total -= livro['preço']
-            else:
-                print(f"O livro {livro_remove} não foi encontrado!")
+            for livro in biblioteca[cat_remove]:
+                cont_remove += 1   
+                if livro['nome'] == livro_remove:
+                    biblioteca[cat_remove].remove(livro)
+                    print(f"\nLivro '{livro_remove}' removido com sucesso!")
+                    preco_total -= livro['preço']
+                    cont_remove -= 100000
+                    
+            if cont_remove > 0:
+                    print(f"\nO livro {livro_remove} não foi encontrado!")
         else:
-            print(f"A categoria {cat_remove} não foi encontrada!")
+            print(f"\nA categoria {cat_remove} não foi encontrada!")
 
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
     elif mod == 3:
-        livro_subst = print("Digite o livro que você deseja atualizar: ")
+        os.system("cls")
+        print("Categorias disponiveis: \n")
+
+        cont_mudar = 0
+
+        for x in range(cont):
+            print('-',lista_cat[x])
+
+        print()
         
-    
+        cat_atualizada = input("Digite a categoria do livro que você deseja atualizar: ") 
+        
+        if cat_atualizada in biblioteca:
+            print(f"\nCategoria: {cat_atualizada}")
+            print("\nLivros:\n")
+            
+            for livro in biblioteca[cat_atualizada]:
+                print(f"Nome: {livro['nome']}")
+                
+            livro_novo = input("\nDigite o livro que você deseja atualizar: ")  
+
+            for livro in biblioteca[cat_atualizada]:    
+                cont_mudar += 1
+                if livro['nome'] == livro_novo:
+                    opcao_atualizar = input("\nDigite o que você deseja mudar:\n- Nome[N];\n- Autor[A];\n- Preço[P]\nSua opção: ")
+                    if opcao_atualizar == 'N':
+                        livro['nome'] = input("\nNovo nome do livro: ")
+                    elif opcao_atualizar == 'A':
+                        livro['autor'] = input("\nNovo autor do livro:")
+                    elif opcao_atualizar == 'P':
+                        livro['preço'] = int(input("\nNovo preço do livro: "))
+                    print(f"\nLivro '{livro_novo}' atualizado com sucesso!\n")
+                    cont_mudar -= 10000000
+                                                        
+                    if cont_mudar > 0:
+                        print("\nLivro não encontrado!")
+
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
     elif mod == 4:
@@ -105,8 +144,19 @@ while True:
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
     elif mod == 5:
-        print("\nDinheiro total gasto: R$", preco_total)
-    
+        
+        preco_total = sum(livro['preço'] for categoria in biblioteca for livro in biblioteca[categoria])
+        print("\nDinheiro total gasto em todos os livros: R$",preco_total)
+
+    #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
     elif mod == 6:
-        print("Saindo do programa. Obrigado!")
+        print("wqe")
+        #Carrinho#
+    
+    #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+    elif mod == 7:
+        print("Saindo do programa. Obrigado!\n")
+        print("---------Volte sempre!---------\n")
         break
